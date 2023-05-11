223f23e7a1267742899e6c93a8e43699d0cfbe3c
Rygone <44498456+Rygone@users.noreply.github.com>
2023-05-11 08:45:09 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --chdir /scratch/izar/plumey
+#SBATCH --chdir /scratch/izar/plumey/ProjectRepository
 #SBATCH --nodes 1
 #SBATCH --ntasks 1
 #SBATCH --cpus-per-task 4
@@ -8,7 +8,7 @@
 #SBATCH --gres gpu:1
 #SBATCH --qos dlav
 #SBATCH --account civil-459-2023
-#SBATCH --reservation civil-459 -> can be removed if we want to used outside of the exercise ession
+#SBATCH --reservation civil-459
 
 source venvs/venv-g21/bin/activate
 python3 train.py
