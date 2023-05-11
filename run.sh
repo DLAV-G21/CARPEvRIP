861049b6e0289d2bdef0800bd8c8080a0a3da308
Rygone <44498456+Rygone@users.noreply.github.com>
2023-05-11 11:09:06 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --chdir /scratch/izar/plumey/ProjectRepository
+#SBATCH --chdir /scratch/izar/plumey
 #SBATCH --nodes 1
 #SBATCH --ntasks 1
 #SBATCH --cpus-per-task 4
@@ -8,7 +8,10 @@
 #SBATCH --gres gpu:1
 #SBATCH --qos dlav
 #SBATCH --account civil-459-2023
+#SBATCH --reservation civil-459
+
 
 source venvs/venv-g21/bin/activate
 echo ici > ici.txt
+cd /ProjectRepository
 python3 train.py
